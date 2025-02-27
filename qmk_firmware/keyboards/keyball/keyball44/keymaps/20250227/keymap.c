1d68b8074c8532ae44d230a007048db5d7249361
j-baba <7057323+j-baba@users.noreply.github.com>
2025-02-27 22:30:42 +0900

    modify keymap.c

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/20250227/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/20250227/keymap.c
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [3] = LAYOUT_universal(
-    RGB_TOG  , AML_TO   , AML_I50  , AML_D50  , _______  , _______  ,                                        RGB_M_P  , RGB_M_B  , RGB_M_R  , RGB_M_SW , RGB_M_SN , RGB_M_K  ,
+    RGB_TOG  , AML_TO   , AML_I50  , AML_D50  , LAY_TOG  , _______  ,                                        RGB_M_P  , RGB_M_B  , RGB_M_R  , RGB_M_SW , RGB_M_SN , RGB_M_K  ,
     RGB_MOD  , RGB_HUI  , RGB_SAI  , RGB_VAI  , _______  , SCRL_DVI ,                                        RGB_M_X  , RGB_M_G  , RGB_M_T  , RGB_M_TW , _______  , _______  ,
     RGB_RMOD , RGB_HUD  , RGB_SAD  , RGB_VAD  , _______  , SCRL_DVD ,                                        CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  , _______  , KBC_SAVE ,
                   QK_BOOT  , KBC_RST  , _______  ,        _______  , _______  ,                   _______  , _______  , _______       , KBC_RST  , QK_BOOT
@@ -56,38 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 layer_state_t layer_state_set_user(layer_state_t state) {
     // Auto enable scroll mode when the highest layer is 3
     keyball_set_scroll_mode(get_highest_layer(state) == 3);
-    return state;
-}
-
-#ifdef OLED_ENABLE
-
-#    include "lib/oledkit/oledkit.h"
-
-void oledkit_render_info_user(void) {
-    keyball_oled_render_keyinfo();
-    keyball_oled_render_ballinfo();
-    keyball_oled_render_layerinfo();
-}
-#endif
-
-#ifdef LAYER_LED_ENABLE
-#include "layer_led.c"
-#endif
-
-enum my_keyball_keycodes {
-    LAY_TOG = KEYBALL_SAFE_RANGE,
-};
-
-// キーマップの任意の場所に「LAY_TOG」を追加(機能の有効無効切り替えキー)
-// 例：
-//  [3] = LAYOUT_universal(
-//    RGB_TOG  , LAY_TOG  ,
-
-layer_state_t layer_state_set_user(layer_state_t state) {
-    // 既存処理 ...
-
     change_layer_led_color(state);
-
     return state;
 }
 
@@ -101,3 +70,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     }
     return true;
 }
+
+#ifdef OLED_ENABLE
+
+#    include "lib/oledkit/oledkit.h"
+
+void oledkit_render_info_user(void) {
+    keyball_oled_render_keyinfo();
+    keyball_oled_render_ballinfo();
+    keyball_oled_render_layerinfo();
+}
+#endif
